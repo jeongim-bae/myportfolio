d7c5d453e3ecc39d5740086979684ceadc5bc600
Jeongim Bae <jeongim.bae@powercoders.org>
2020-06-23 22:07:18 +0200

    connect to the database for the project section

--- routes/main.js
+++ routes/main.js
@@ -1,8 +1,19 @@
 const vertex = require("vertex360")({ site_id: process.env.TURBO_APP_ID });
 const router = vertex.router();
+const ProjectController = require("../controllers/ProjectController.js");
 
 router.get("/", (req, res) => {
   const data = req.context;
-  res.render("landing", data);
+  const projectCtr = new ProjectController();
+  projectCtr
+    .get()
+    .then((projects) => {
+      data["projects"] = projects;
+      res.render("landing", data);
+    })
+    .catch((err) => {
+      res.send("Oops! +err.message");
+    });
 });
+
 module.exports = router;
